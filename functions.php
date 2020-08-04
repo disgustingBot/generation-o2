6c2171a0c24f06b48525f4abafcee5ed1953dd73
molinerozadkiel <50532987+molinerozadkiel@users.noreply.github.com>
2020-08-04 12:45:57 +0200

    cosas

--- functions.php
+++ functions.php
@@ -138,71 +138,156 @@ add_action( 'init', 'register_menus' );
 
 
 
-
 // FUCTION FOR USER GENERATION
 // https://tommcfarlin.com/create-a-user-in-wordpress/
-add_action( 'admin_post_nopriv_lt_login', 'lt_login');
-add_action(        'admin_post_lt_login', 'lt_login');
-function lt_login(){
-  $link=$_POST['link'];
-  $name=$_POST['name'];
-  $fono=$_POST['fono'];
-  $mail=$_POST['mail'];
-  $pass=$_POST['pass'];
+add_action( 'admin_post_nopriv_lt_sumate', 'lt_sumate');
+add_action(        'admin_post_lt_sumate', 'lt_sumate');
+function lt_sumate(){
+  $link          = $_POST['link'];
+  $nombre        = $_POST['nombre'];
+  $apellido      = $_POST['apellido'];
+  $dni           = $_POST['dni'];
+  $email         = $_POST['email'];
+  $telefono      = $_POST['telefono'];
+  $ciudad        = $_POST['ciudad'];
+  $codigo_postal = $_POST['codigo_postal'];
+  $domicilio     = $_POST['domicilio'];
+  $socio         = $_POST['socio'];
+  $voluntario    = $_POST['voluntario'];
+  $cantidad      = $_POST['cantidad'];
+  $dia_de_pago   = $_POST['dia_de_pago'];
+  $cuenta        = $_POST['cuenta'];
+  $departamento  = $_POST['departamento'];
+  $nota          = $_POST['nota'];
+  // var_dump($_POST);
+  // echo '<br>';
+  // echo '<br>';
+  // var_dump($_GET);
+  // echo '<br>';
+  // echo '<br>';
+  // var_dump($apellido);
+  // echo '<br>';
+  // echo '<br>';
+  // var_dump($email);
+  // echo '<br>';
+  // echo '<br>';
 
 
-  if( null == username_exists( $mail ) ) {
+    // ERROR HANDLING
+  if( null != username_exists( $mail ) || email_exists( $mail ) != false  ) {
+    $action='alreadyExist';
+  } else {
 
-    // Generate the password and create the user for security
-    // $password = wp_generate_password( 12, false );
-    // $user_id = wp_create_user( $mail, $password, $mail );
-
-    // user generated pass for local testing
-    $user_id = wp_create_user( $mail, $pass, $mail );
-    // Set the nickname and display_name
-    wp_update_user(
-      array(
-        'ID'              =>    $user_id,
-        'display_name'    =>    $name,
-        'nickname'        =>    $name,
-      )
-    );
-    update_user_meta( $user_id, 'phone', $fono );
+    // recaptcha
 
 
-    // Set the role
-    $user = new WP_User( $user_id );
-    $user->set_role( 'subscriber' );
+    // $site = '6LcRuNAUAAAAADtamJW75fYf8YtNHceSngjKsf-B';
+    // $scrt = '6LcRuNAUAAAAALBu7Ymh0yxmTXTJmP0rsnkjGyj0';
 
-    // Email the user
-    wp_mail( $mail, 'Welcome '.$name.'!', 'Your Password: ' . $pass );
-  // end if
-  $action='register';
-  $creds = array(
-      'user_login'    => $mail,
-      'user_password' => $pass,
-      'remember'      => true
-  );
+    // $response = $_POST['g-recaptcha-response'];
+    // $payload = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$scrt.'&response='.$response);
+    // // echo $payload;
+    // $result = json_decode($payload,true);
+    // if ($result['success']!=1) {
+    //    // code...
+    //    // header( "Location: https://multiviahr.info/?mail=BOT" );
+    //    // $link = add_query_arg( array( 'status' => 'bot' , ), $link );
+    //    // echo 'you are evil or a bott';
+    //    // exit;
+    //     $action='bot';
+    //    // return false;
+    // } else {
 
-  $status = wp_signon( $creds, false );
-} else {
 
-  $creds = array(
-      'user_login'    => $mail,
-      'user_password' => $pass,
-      'remember'      => true
-  );
 
-  $status = wp_signon( $creds, false );
+      // Generate the password and create the user for security
+      $password = wp_generate_password( 12, false );
+      // $user_id = wp_create_user( $mail, $password, $mail );
 
-  // $status=wp_login($mail, $pass);
+      // user generated pass for local testing
 
-  $action='login';
-}
+      $user_nicename = sanitize_title($nombre . '_' . $apellido);
+      echo $new_url;
+
+      $userdata = array(
+        'user_pass'             => $password,   //(string) The plain-text user password.
+        'user_login'            => $email,   //(string) The user's login username.
+        'user_nicename'         => $user_nicename,   //(string) The URL-friendly user name.
+        // 'user_url'              => '',   //(string) The user URL.
+        'user_email'            => $email,   //(string) The user email address.
+        'display_name'          => $nombre,   //(string) The user's display name. Default is the user's username.
+        'nickname'              => $nombre,   //(string) The user's nickname. Default is the user's username.
+        'first_name'            => $nombre,   //(string) The user's first name. For new users, will be used to build the first part of the user's display name if $display_name is not specified.
+        'last_name'             => $apellido,   //(string) The user's last name. For new users, will be used to build the second part of the user's display name if $display_name is not specified.
+        // 'description'           => '',   //(string) The user's biographical description.
+        // 'rich_editing'          => '',   //(string|bool) Whether to enable the rich-editor for the user. False if not empty.
+        // 'syntax_highlighting'   => '',   //(string|bool) Whether to enable the rich code editor for the user. False if not empty.
+        // 'comment_shortcuts'     => '',   //(string|bool) Whether to enable comment moderation keyboard shortcuts for the user. Default false.
+        // 'admin_color'           => '',   //(string) Admin color scheme for the user. Default 'fresh'.
+        // 'use_ssl'               => '',   //(bool) Whether the user should always access the admin over https. Default false.
+        // 'user_registered'       => '',   //(string) Date the user registered. Format is 'Y-m-d H:i:s'.
+        // 'show_admin_bar_front'  => '',   //(string|bool) Whether to display the Admin Bar for the user on the site's front end. Default true.
+        'role'                  => 'subscriber',   //(string) User's role.
+        // 'locale'                => '',   //(string) User's locale. Default empty.
+      );
+
+      $user_id = wp_insert_user( $userdata );
+
+
+      // $user_id = wp_create_user( $nombre, $password, $email );
+      // Set the nickname and display_name
+      // wp_update_user(
+      //   array(
+      //     'ID'              =>    $user_id,
+      //     'display_name'    =>    $nombre,
+      //     'nickname'        =>    $nombre,
+      //   )
+      // );
+      update_user_meta( $user_id, 'telefono', $telefono );
+      update_user_meta( $user_id, 'dni', $dni );
+      update_user_meta( $user_id, 'ciudad', $ciudad );
+      update_user_meta( $user_id, 'codigo_postal', $codigo_postal );
+      update_user_meta( $user_id, 'domicilio', $domicilio );
+      update_user_meta( $user_id, 'socio', $socio );
+      update_user_meta( $user_id, 'voluntario', $voluntario );
+      update_user_meta( $user_id, 'cantidad', $cantidad );
+      update_user_meta( $user_id, 'dia_de_pago', $dia_de_pago );
+      update_user_meta( $user_id, 'cuenta', $cuenta );
+      update_user_meta( $user_id, 'departamento', $departamento );
+      update_user_meta( $user_id, 'nota', $nota );
+
+      
+      // $hash = hash ( 'sha256' , time() . $mail );
+      // update_user_meta( $user_id, 'confirmation', $hash );
+
+
+      // Set the role
+      // $user = new WP_User( $user_id );
+      // $user->set_role( 'subscriber' );
+
+      // Email the user
+      // $message='';
+      // $message=$message.'Your Password: ' . $pass;
+      // $message=$message.'<br>';
+      // $message=$message.'activation Code: ';
+      // $message=$message.'<br>';
+      // $enlace=get_site_url().'/confirmation/?confirmation='.$hash;
+      // $message=$message.'<a href="'.$enlace.'">'.$enlace.'</a>';
+      // $message=$message.'<br>';
+      // $headers = array('Content-Type: text/html; charset=UTF-8');
+
+      // wp_mail( $mail, 'Welcome '.$name.'!', $message, $headers );
+
+
+      $action='register';
+    // }
+
+  }
 
   $link = add_query_arg( array(
-    'action' => $action,
     // 'status' => $status,
+    'action' => $action,
+    // 'mail'   => $mail,
     // 'resultado' => username_exists( $mail ),
   ), $link );
   wp_redirect($link);
--- page-sumate.php
+++ page-sumate.php
@@ -1,60 +1,62 @@
 <?php get_header(); ?>
 
 
-<form class="sumate_form" action="">
-    <h1>SUMATE</h1>
+<form class="sumate_form" method="POST" action="<?php echo esc_url( admin_url('admin-post.php') ); ?>">
+    <input type="hidden" name="action" value="lt_sumate">
+    <input type="hidden" name="link" value="<?php echo home_url( $wp->request ); ?>">
+    <h1>SÚMATE!</h1>
 
-    <div>
-        <label for="sumate_name">Nombre</label>
-        <input type="text" id="sumate_name">
+    <div class="sumate_imp">
+        <label class="sumate_label" for="sumate_name">Nombre</label>
+        <input class="sumate_input" type="text" name="nombre" id="sumate_name">
     </div>
 
-    <div>
-        <label for="sumate_last">Apellido</label>
-        <input type="text" id="sumate_last">
+    <div class="sumate_imp">
+        <label class="sumate_label" for="sumate_last">Apellido</label>
+        <input class="sumate_input" type="text" name="apellido" id="sumate_last">
     </div>
 
-    <div>
-        <label for="sumate_dni">DNI / NIE</label>
-        <input type="text" id="sumate_dni">
+    <div class="sumate_imp">
+        <label class="sumate_label" for="sumate_dni">DNI / NIE</label>
+        <input class="sumate_input" type="text" name="dni" id="sumate_dni">
     </div>
 
-    <div>
-        <label for="sumate_mail">E-mail</label>
-        <input type="text" id="sumate_mail">
+    <div class="sumate_imp">
+        <label class="sumate_label" for="sumate_mail">E-mail</label>
+        <input class="sumate_input" type="text" name="email" id="sumate_mail">
     </div>
 
-    <div>
-        <label for="sumate_telefono">Telefono</label>
-        <input type="text" id="sumate_telefono">
+    <div class="sumate_imp">
+        <label class="sumate_label" for="sumate_telefono">Telefono</label>
+        <input class="sumate_input" type="text" name="telefono" id="sumate_telefono">
     </div>
 
-    <div>
-        <label for="sumate_ciudad">ciudad</label>
-        <input type="text" id="sumate_ciudad">
+    <div class="sumate_imp">
+        <label class="sumate_label" for="sumate_ciudad">ciudad</label>
+        <input class="sumate_input" type="text" name="ciudad" id="sumate_ciudad">
     </div>
 
-    <div>
-        <label for="sumate_codigo_postal">codigo postal</label>
-        <input type="text" id="sumate_codigo_postal">
+    <div class="sumate_imp">
+        <label class="sumate_label" for="sumate_codigo_postal">codigo postal</label>
+        <input class="sumate_input" type="text" name="codigo_postal" id="sumate_codigo_postal">
     </div>
 
-    <div>
-        <label for="sumate_domicilio">Domicilio</label>
-        <input type="text" id="sumate_domicilio">
+    <div class="sumate_imp">
+        <label class="sumate_label" for="sumate_domicilio">Domicilio</label>
+        <input class="sumate_input" type="text" name="domicilio" id="sumate_domicilio">
     </div>
 
     <div>
         <p>Sumarme como</p>
         <label for="sumate_tipo_socio">Socio</label>
-        <input type="checkbox" id="sumate_tipo_socio">
+        <input type="checkbox" id="sumate_tipo_socio" name="socio">
         <label for="sumate_tipo_voluntario">Voluntario</label>
-        <input type="checkbox" id="sumate_tipo_voluntario">
+        <input type="checkbox" id="sumate_tipo_voluntario" name="voluntario">
     </div>
 
     <div>
         <label for="sumate_cantidad">Donar</label>
-        <select name="sumate_cantidad" id="sumate_cantidad">
+        <select name="sumate_cantidad" id="sumate_cantidad" name="cantidad">
             <option value="10">Cuota 10 euros</option>
             <option value="20">Cuota 20 euros</option>
             <option value="otro">Otra cantidad</option>
@@ -63,21 +65,21 @@
 
     <div>
         <label for="sumate_dia_pago">Dia de pago</label>
-        <select name="sumate_dia_pago" id="sumate_dia_pago">
+        <select name="sumate_dia_pago" id="sumate_dia_pago" name="dia_de_pago">
             <option value="05">El 05 de cada mes</option>
             <option value="10">El 10 de cada mes</option>
             <option value="15">El 15 de cada mes</option>
         </select>
     </div>
 
-    <div>
-        <label for="sumate_cuenta_bancaria">Cuenta bancaria</label>
-        <input type="text" id="sumate_cuenta_bancaria">
+    <div class="sumate_imp">
+        <label class="sumate_label" for="sumate_cuenta_bancaria">Cuenta bancaria</label>
+        <input class="sumate_input" type="text" id="sumate_cuenta_bancaria" name="cuenta">
     </div>
 
     <div>
         <label for="sumate_departamento">Elige</label>
-        <select name="sumate_departamento" id="sumate_departamento">
+        <select name="sumate_departamento" id="sumate_departamento" name="departamento">
             <option value="d_ela">Departamento ELA</option>
             <option value="i_ela">Investigacion ELA</option>
             <option value="i_vih">Investigacion VIH</option>
@@ -88,8 +90,10 @@
 
     <div>
         <label for="sumate_nota">Nota</label>
-        <textarea type="text" id="sumate_nota"></textarea>
+        <textarea type="text" id="sumate_nota" name="nota"></textarea>
     </div>
+
+    <button type="submit">Sumarme</button>
 </form>
 
 <?php get_footer(); ?>
