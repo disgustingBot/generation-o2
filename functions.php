a869e159e8bf5300235622f7d0bff14392cebca7
molinerozadkiel <50532987+molinerozadkiel@users.noreply.github.com>
2020-08-06 19:53:10 +0200

    generalized function to define custom post and taxonomies
    also custom field to taxonomy boilerplate code

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'customPosts.php';
+require_once 'inc/customPosts.php';
 require_once 'inc/user-meta.php';
 
 function lt_scripts(){
@@ -99,6 +99,97 @@ add_action( 'init', 'register_menus' );
 
 
 
+
+  //Product Cat Create page
+  function lt_taxonomy_add_new_meta_field() {
+    ?>
+    <div class="form-field">
+        <label for="lt_meta_img"><?php _e('Imagen destacada', 'lt'); ?></label>
+        <input type="text" name="lt_meta_img" id="lt_meta_img">
+        <p class="description"><?php _e('Imagen destacada de categoria, insertar title de imagen de galeria de medios', 'lt'); ?></p>
+    </div>
+    <?php
+}
+
+//Product Cat Edit page
+function lt_taxonomy_edit_meta_field($term) {
+
+    //getting term ID
+    $term_id = $term->term_id;
+
+    // retrieve the existing value(s) for this meta field.
+    $lt_meta_img = get_term_meta($term_id, 'lt_meta_img', true);
+    ?>
+    <tr class="form-field">
+        <th scope="row" valign="top"><label for="lt_meta_img"><?php _e('Imagen destacada', 'lt'); ?></label></th>
+        <td>
+          <input type="text" name="lt_meta_img" id="lt_meta_img" value="<?php echo get_term_meta($term_id, 'lt_meta_img', true); ?>">
+            <p class="description"><?php _e('Imagen destacada de categoria, insertar title de imagen de galeria de medios', 'lt'); ?></p>
+        </td>
+    </tr>
+    <?php
+}
+
+// add_action('add_category_form_fields', 'lt_taxonomy_add_new_meta_field', 10, 1);
+// add_action('edit_category_form_fields', 'lt_taxonomy_edit_meta_field', 10, 1);
+add_action('departamento_edit_form_fields','lt_taxonomy_edit_meta_field');
+add_action('departamento_add_form_fields','lt_taxonomy_add_new_meta_field');
+
+// Save extra taxonomy fields callback function.
+function lt_save_taxonomy_custom_meta($term_id) {
+  // if ( isset( $_POST['lt_meta_img'] ) ) {
+  //     update_term_meta($_POST['tag_ID'], 'lt_meta_img', $_POST['lt_meta_img']);
+  // }
+
+    $lt_meta_img = filter_input(INPUT_POST, 'lt_meta_img');
+
+    update_term_meta($term_id, 'lt_meta_img', $lt_meta_img);
+    // update_term_meta(4, 'lt_meta_img', 'sin testear no se llega a ningun lado');
+}
+// function saveCategoryFields() {
+//   if ( isset( $_POST['cat_title'] ) ) {
+//       update_term_meta($_POST['tag_ID'], '_pagetitle', $_POST['cat_title']);
+//   }
+// }
+
+add_action('edited_departamento', 'lt_save_taxonomy_custom_meta', 10, 1);
+add_action('create_departamento', 'lt_save_taxonomy_custom_meta', 10, 1);
+
+
+
+// add_action('departamento_edit_form_fields','addTitleFieldToCat');
+// add_action('departamento_add_form_fields','addTitleFieldToCat');
+// add_action('departamento_edit_form', 'addTitleFieldToCat');
+// add_action('departamento_add_form','addTitleFieldToCat');
+
+
+
+
+
+
+
+
+function addTitleFieldToCat(){
+  $cat_title = get_term_meta($_POST['tag_ID'], '_pagetitle', true);
+  ?> 
+  <tr class="form-field">
+      <th scope="row" valign="top"><label for="cat_page_title"><?php _e('Category Page Title'); ?></label></th>
+      <td>
+      <input type="text" name="cat_title" id="cat_title" value="<?php echo $cat_title ?>"><br />
+          <span class="description"><?php _e('Title for the Category '); ?></span>
+      </td>
+  </tr>
+  <?php
+
+}
+// add_action ( 'edit_category_form_fields', 'addTitleFieldToCat');
+
+function saveCategoryFields() {
+  if ( isset( $_POST['cat_title'] ) ) {
+      update_term_meta($_POST['tag_ID'], '_pagetitle', $_POST['cat_title']);
+  }
+}
+// add_action ( 'edited_category', 'saveCategoryFields');
 
 
 
---
+++ inc/customPosts.php
@@ -0,0 +1,99 @@
+<?php
+// Built by www.lattedev.com
+function lt_new_custom_post($name, $icon = '', $taxonomies = array() ){
+	$last_letter = substr($name, -1);
+	if( in_array( $last_letter, array('a','e','i','o','u') ) ){
+		$name_pl    = $name.'s';
+	} else {
+		$name_pl    = $name.'es';
+	}
+	$name_M     = ucfirst($name);
+	$name_M_pl  = ucfirst($name_pl);
+	
+    /* Añado las etiquetas que aparecerán en el escritorio de WordPress */
+	$labels = array(
+		'name'               => _x( $name_M_pl, 'post type general name', 'lt-domain' ),
+		'singular_name'      => _x( $name_M, 'post type singular name', 'lt-domain' ),
+		'menu_name'          => _x( $name_M_pl, 'admin menu', 'lt-domain' ),
+		'add_new'            => _x( 'Añadir nuevo', $name, 'lt-domain' ),
+		'add_new_item'       => __( 'Añadir nuevo ' . $name, 'lt-domain' ),
+		'new_item'           => __( 'Nuevo ' . $name, 'lt-domain' ),
+		'edit_item'          => __( 'Editar ' . $name, 'lt-domain' ),
+		'view_item'          => __( 'Ver ' . $name, 'lt-domain' ),
+		'all_items'          => __( 'Todos les ' . $name_pl, 'lt-domain' ),
+		'search_items'       => __( 'Buscar ' . $name_pl, 'lt-domain' ),
+		'not_found'          => __( 'No hay ' . $name_pl, 'lt-domain' ),
+		'not_found_in_trash' => __( 'Ningun ' . $name . ' en la papelera.', 'lt-domain' )
+	);
+	
+    /* Configuro el comportamiento y funcionalidades del nuevo custom post type */
+	$args = array(
+		'labels'             => $labels,
+		'description'        => __( 'Desctripción.', 'lt-domain' ),
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_nav_menus'  => true,
+		'show_in_menu'       => true,
+		'show_in_rest'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => $name ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'taxonomies'         => $taxonomies,
+		'menu_icon'          => $icon,
+		'menu_position'      => 5,
+		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments', 'revisions' )
+	);
+	register_post_type( $name, $args );
+
+
+	foreach ($taxonomies as $key => $tax_name) {
+
+		$last_letter = substr($tax_name, -1);
+		if( in_array( $last_letter, array('a','e','i','o','u') ) ){
+			$tax_name_pl    = $tax_name.'s';
+		} else {
+			$tax_name_pl    = $tax_name.'es';
+		}
+		$tax_name_M     = ucfirst($tax_name);
+		$tax_name_M_pl  = ucfirst($tax_name_pl);
+		# code...
+		/* Configuramos las etiquetas que mostraremos en el escritorio de WordPress */
+		$labels = array(
+			'name'             => _x( $tax_name_M_pl, 'taxonomy general name', 'lt-domain' ),
+			'singular_name'    => _x( $tax_name_M, 'taxonomy singular name', 'lt-domain' ),
+			'search_items'     => __( 'Buscar por ' . $tax_name, 'lt-domain' ),
+			'all_items'        => __( 'Todos les ' . $tax_name_pl, 'lt-domain' ),
+			'parent_item'      => __( $tax_name_M . ' padre', 'lt-domain' ),
+			'parent_item_colon'=> __( $tax_name_M . ' padre:', 'lt-domain' ),
+			'edit_item'        => __( 'Editar ' . $tax_name, 'lt-domain' ),
+			'update_item'      => __( 'Actualizar ' . $tax_name_pl, 'lt-domain' ),
+			'add_new_item'     => __( 'Añadir nuevo ' . $tax_name, 'lt-domain' ),
+			'new_item_name'    => __( 'Nombre del nuevo ' . $tax_name, 'lt-domain' ),
+		);
+
+		/* Registramos la taxonomía y la configuramos como jerárquica (al estilo de las categorías) */
+		register_taxonomy( $tax_name, array( $name ), array(
+			'labels'             => $labels,
+			'public'             => true,
+			'hierarchical'       => true,
+			'show_ui'            => true,
+			'query_var'          => true,
+			'show_in_nav_menus'  => true,
+			'show_admin_column'  => true,
+			'show_in_rest'       => true, // Needed for tax to appear in Gutenberg editor.
+			'rewrite'            => array( 'slug' => $tax_name ),
+		));
+
+	}
+
+}
+
+add_action( 'init', 'lt_custom_posts' );
+function lt_custom_posts() {
+	lt_new_custom_post( 'proyecto', 'dashicons-admin-site-alt3', array('departamento') );
+	lt_new_custom_post( 'equipo', 'dashicons-groups', array( 'area', 'departamento', 'cargo' ) );
+	lt_new_custom_post( 'colaborador', 'dashicons-heart', array('entidad') );
+}
