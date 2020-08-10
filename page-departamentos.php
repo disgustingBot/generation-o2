aa83d9629fa8390f725f3850cdc5cf61232994f8
molinerozadkiel <50532987+molinerozadkiel@users.noreply.github.com>
2020-08-10 12:45:51 +0200

    casi lista porque no necesita ser responsive

--- page-departamentos.php
+++ page-departamentos.php
@@ -3,7 +3,6 @@
 
 <section class="departamentos">
   <h1><?php echo get_the_title(); ?></h1>
-</section>
 <?php
 
 $terms = get_terms( array(
@@ -13,14 +12,12 @@ $terms = get_terms( array(
 
 // Loop through all terms with a foreach loop
 foreach( $terms as $term ) { ?>
-  <article class="card">
-    <img data-url="<?php echo wp_get_attachment_url( get_img_id_by_slug(get_term_meta( $term->term_id, 'lt_meta_img', true ))); ?>" alt="" class="card_img card_img_rounded lazy">
-    <hgroup class="cardCaption">
-      <h4 class="cardCaption_title"><?php echo $term->name; ?></h4>
-      <h5 class="cardCaption_txt"><?php echo $term->description; ?></h5>
-    </hgroup>
+  <article class="deep">
+    <img class="deep_img lazy" data-url="<?php echo wp_get_attachment_url( get_img_id_by_slug(get_term_meta( $term->term_id, 'lt_meta_img', true ))); ?>" alt="">
+    <h3 class="deep_btn" style="--background_from_php:rgba(var(--<?php echo $term->slug; ?>_color_rgb), .8)"><a href=""><?php echo $term->name; ?></a></h3>
   </article>
 <?php } ?>
+</section>
 
 
 
