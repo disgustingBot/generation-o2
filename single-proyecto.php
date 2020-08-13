083721ae1a2f53bd27d071dda843ddfb558081f9
tomasmparra <50798104+tomasmparra@users.noreply.github.com>
2020-08-13 19:09:53 +0200

    sfsdfsdf

---
+++ single-proyecto.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<section class="grid">
+  <img class="lazy" data-url="<?php echo get_the_post_thumbnail_url(); ?>" alt="Ilustración Generación o2">
+
+
+</section>
+
+
+
+
+<?php get_footer(); ?>
