0f3539ceb4d49a3ddb284eaa7f80e455bde214ac
tomasmparra <50798104+tomasmparra@users.noreply.github.com>
2020-08-15 11:30:25 +0200

    Summary

--- single-proyecto.php
+++ single-proyecto.php
@@ -6,11 +6,17 @@
   <div class="overlap_card_caption rowcol1">
     <hgroup class="">
       <h1 class="overlap_card_title"><?php the_title(); ?></h1>
-      <h5 class="overlap_card_author"><i><?php the_author(); ?></i></h5>
+      <h5 class="overlap_card_author"><i>Por <?php the_author(); ?></i></h5>
     </hgroup>
     <div class="vertical_line"></div>
+    <div class="overlap_card_social_container grid">
+      <p class="overlap_card_social_title">Comparte</p>
+      <?php  include 'inc/socialmedia.php'; ?>
+    </div>
   </div>
-
+</section>
+<section class="single_content section_padding">
+  <?php the_content() ?>
 </section>
 
 
